24d767b9fe58eca4c0ab6d991dc5501913165af8
Jek Hales <halesyy@gmail.com>
2017-05-16 22:59:19 +1000

    Added sick animations
    XD

--- assets/js/APP.js
+++ assets/js/APP.js
@@ -14,5 +14,27 @@ var APP = {
         }
       }
     }, 4000);
+    return this;
+  },
+  begin: function() {
+    $sliders = $('.slideonload');
+    $avatar = $('#me');
+    $me = $('#me_container');
+
+    //fade in avatar then transition to left.
+    $avatar.fadeIn(1000, function(){
+      $sliders.css({'width':'0'});//animation.
+    });
+
+    //binding, loading in the first box data.
+    $sliders.bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+      $me.css({'float':'left'});
+      $sliders.css({'width':''});
+
+      displayDataBox1 = setTimeout(function(){
+        $db1 = $('#databox-1');
+        $db1.fadeIn(250);
+      }, 1000);
+    });
   }
 };
